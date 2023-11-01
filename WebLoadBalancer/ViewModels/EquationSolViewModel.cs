da94ac8736f4c87dcbd7c3aaf17522b1afe25f5a
OlenaMikhailova <olenamikhailova14@gmail.com>
2023-11-01 17:44:43 +0200

    Create controller for solving sustem of equations and its view

---
+++ WebLoadBalancer/ViewModels/EquationSolViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using WebLoadBalancer.Attributes;
+
+namespace WebLoadBalancer.ViewModels
+{
+    public class EquationSolViewModel
+    {
+        [Display(Name = "Your file with the system of equations")]
+        [Required(ErrorMessage = "Select the file with the system of equations")]
+        [DataType(DataType.Upload)]
+        [ValidFileExtensions(".txt", ".csv")]
+        public IFormFile EquationFile { get; set; }
+    }
+}
