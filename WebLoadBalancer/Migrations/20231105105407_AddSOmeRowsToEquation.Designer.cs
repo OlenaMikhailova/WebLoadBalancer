78fe6d3da6093bc4a94b6568b79db7614bc88f3e
OlenaMikhailova <olenamikhailova14@gmail.com>
2023-11-05 22:57:07 +0200

    Fix solution view, add history page, try create progress bar

--- WebLoadBalancer/Migrations/20231105105407_AddSOmeRowsToEquation.Designer.cs
+++ WebLoadBalancer/Migrations/20231105105407_AddSOmeRowsToEquation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebLoadBalancer.Models;
@@ -11,9 +12,11 @@ using WebLoadBalancer.Models;
 namespace WebLoadBalancer.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20231105105407_AddSOmeRowsToEquation")]
+    partial class AddSOmeRowsToEquation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
